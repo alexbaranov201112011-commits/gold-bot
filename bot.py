12d119d750e4ef036f8393b7703428f6bc8c791c
alexbaranov201112011-commits <alexbaranov201112011@gmail.com>
2026-09-26 21:24:20 +0500

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import threading, os
-app = Flask(__name__)
-@app.route('/')
-def home(): return "Bot is alive!"
-def run_flask():
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
-threading.Thread(target=run_flask, daemon=True).start()
